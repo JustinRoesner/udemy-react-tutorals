907dd18a638d755a89a1ad6d13f5ae71cc19349a
Justin <justinroesner@gmail.com>
2021-03-10 22:30:32 -0600

    initial push boiler plate

---
+++ 8 blog/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+//this tricks redux temporary that i have a valid reducer during boiler plate code
+export default combineReducers({
+    replaceMe: () => 9999
+});
